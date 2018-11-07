ef6c342e3fb52f07913b9f83402f4f8ea201a0ec
APEX\zack.later <zack.later@vivint.com>
2018-11-07 15:49:41 -0700

    new blackjack

--- BlackJack/Helper/Helper.cs
+++ BlackJack/Helper/Helper.cs
@@ -1,31 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-namespace BlackJack
+namespace _21
 {
-    
-    class Helper
-    
+   
+    public class CreateDeck
     {
-        Dictionaries dictionary = new Dictionaries();
-        Random rand = new Random();
-        public KeyValuePair<string, int> GetRandomCard()
+        private Dictionary<string, int> CardNames = new Dictionary<string, int>()
         {
-           var data = dictionary.cardDeck;
-           return data.ElementAt(rand.Next(0, data.Count));
-        }
+            {"Jack", 10},
+            {"Queen", 10},
+            {"King", 10},
+            {"Ace", 1},
+            {"Two", 2},
+            {"Three", 3},
+            {"Four", 4},
+            {"Five", 5},
+            {"Six", 6},
+            {"Seven", 7},
+            {"Eight", 8},
+            {"Nine", 9},
+            {"Ten", 10},
+        };
+        private List<string> CardTypes = new List<string>()
+        {
+            "Spades", "Hearts", "Clubs", "Diamonds"
+        };
 
-        public void Dealing()
+        public List<Card> Deck = new List<Card>();
+
+        public CreateDeck()
         {
-            var deal = GetRandomCard();
+            foreach (var type in CardTypes)
+            {
+                foreach (var name in CardNames)
+                {
+                    Deck.Add(new Card()
+                    {
+                        CardType = type,
+                        CardName = name.Key,
+                        DisplayName = $"{name.Key} of {type}",
+                        CardValue = name.Value,
+                    });
+                }
+            }
+        }
+    }
+
+    public class Card
+    {
+        public string CardType { get; set; }
+        public int CardValue { get; set; }
+        public string CardName { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class BlackJack
+
+    {
+        public List<string> User = new List<string>();
+        public List<string> Dealer = new List<string>();
+        public void Deal()
+        {
+            Random rand = new Random();
+            var cardDealt = new Card().DisplayName;
+            var deck = new CreateDeck().;
+            deck.
+
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine(GetRandomCard());
             }
+
+            for (int i = 0; i < 2; i++)
+            {
+                User.Add(cardDealt);
+                Dealer.Add(cardDealt);
+            }
+            Console.WriteLine(User[0]);
+            Console.WriteLine(User[1]);
+            Console.WriteLine(Dealer[0]);
+            Console.WriteLine(Dealer[1]);
+
+
 
         }
     }
